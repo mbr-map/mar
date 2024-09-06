209b6b708c738fa8f0601046473f59f2880d501f
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-06 00:56:59 +0000

    new code added

--- DBUtils.java
+++ DBUtils.java
@@ -1,7 +1,19 @@
-echo " This file contains the DB connection logic
-Im in updated the  master branch
+#!/bin/bash
+echo " This file contains the DB connection logic"
+#Im in updated the  master branch
 
 echo " Im modifiying and updating code.."
 echo " Im modifiying code in master branch......"
 
-im updating the file in developement branch"
+#im updating the file in developement branch"
+
+echo " Updating the new code......"
+echo " Check if a number is even or odd? "
+read -p " Enter a number :" num
+if [[ $(( num % 2 )) == 0 ]]
+then
+echo " $num  is a evennumber"
+else
+echo " $num is a oddnumber"
+fi
+
