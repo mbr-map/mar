068f5c3f68e3c8b3b6ccd4a11693e4283a46cd06
bharathmbr <113525016+bharathmbr@users.noreply.github.com>
2024-09-08 10:15:27 +0530

    Update Deploy.java

--- Deploy.java
+++ Deploy.java
@@ -3,3 +3,13 @@
 
 hello im from developement branch im updating code
 hello im updated code from developement branch
+
+  #write a shell script to print 1 to 10 by using the whileloop
+  echo " while loop staring..."
+  a=1
+  while(a -lt 10)
+  do
+    echo "$a loop"
+  ((a--))
+  done
+  echo "while loop ended"
