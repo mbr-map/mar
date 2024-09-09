c8b01a60a46309b0dd8d7443e39e5fdd39216a04
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-09 17:15:03 +0000

    update in stage

--- example5.sh
+++ example5.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+echo " write a shell script to check the dir is existence or not "
+read -p " Please enter the dir-name :" dir
+if [[ -r $dir ]]
+then
+	echo "$dir is existence.."
+else
+	echo "$dir is not existence"
+fi
 
