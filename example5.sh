106da6c4586405d2cf2284453d4cbebe40a88430
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-10 03:19:20 +0000

    taskno:1 taskname:ex5

--- example5.sh
+++ example5.sh
@@ -7,4 +7,21 @@ then
 else
 	echo "$dir is not existence"
 fi
+echo "----------------------------------------------------"
+echo " UPdating code from stage branch...................."
+
+#write a shell Script to check the file having read permission or not ? if not have set read  permission ?
+
+echo " Check weather read permission or not .."
+read -p " Please enter the filename :" filename
+ if [[ -r $filename ]]
+ then
+	 echo " $filename this file have read permission"
+ else
+	 echo " $file this file not have read permission"
+	 echo " setting the read permission to the file."
+	 chmod +r $filename
+ fi
+
+ echo "----------------------------------------------------------------------"
 
