1e2da8be27756d3c85cab96cb019e5017eae34c0
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-17 03:02:50 +0000

    features-branch Updated

--- example8.sh
+++ example8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #write a shell script to print the number from 1 t0 10 by using while loop
-#write a shell script to print the number from 10 t0 0 by using while loop
+#write a shell script to print the number from 10 to 0 by using while loop
 
 echo" printing 1 to 10 using while-loop "
 echo " While loop starting............."
@@ -12,5 +12,16 @@ do
 done
 echo " This code updated from developement branch........"
 echo " This code is updated from master branch..."
-echo " To fix the bug in the bugfix branch
+echo " To fix the bug in the bugfix branch"
+echo "--------------------------------------------------------------------"
+echo " This code updating from features branch........"
+
+echo "while looping starting........"
+a=10
+while (( $a >= 0 ))
+do
+	echo "$a"
+	(( a-- ))
+done
+echo " While loop is ending..........."
 
